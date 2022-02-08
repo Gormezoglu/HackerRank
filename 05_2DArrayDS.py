883a817735fcfa19fa115a8ff7e1a08450c9811e
Melih <melih3553@gmail.com>
2022-02-08 14:20:35 +0300

    Solution added

---
+++ 05_2DArrayDS.py
@@ -0,0 +1,14 @@
+    
+def hourglassSum(arr): 
+    
+    k = []
+    for i in range(4):
+        for j in range (4):            
+            
+            res = arr[i][j]+ arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr [i+2][j] + arr [i+2][j+1] +                 arr [i+2][j+2]        
+            k.append(res)
+        
+    return max(k)
+    
+
+    
